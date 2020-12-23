321863c0337f2736f68a68b6453e43ceef47cf21
Kacper Surowka <kacper.surowka@pl.abb.com>
2020-12-23 22:19:53 +0100

    Updated JDBCPlaygroundTest

--- src/test/java/pl/ksurowka/voucherstore/productcatalog/JDBCProductStorageTest.java
+++ src/test/java/pl/ksurowka/voucherstore/productcatalog/JDBCProductStorageTest.java
@@ -1,6 +1,11 @@
 package pl.ksurowka.voucherstore.productcatalog;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 import pl.ksurowka.voucherstore.productcatalog.JDBCProductStorage;
 import pl.ksurowka.voucherstore.productcatalog.Product;
 import pl.ksurowka.voucherstore.productcatalog.ProductStorage;
@@ -10,13 +15,19 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class JDBCProductStorageTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     public void AllowsStoringProduct() {
         Product p1 = thereIsProduct();
         ProductStorage storage = new JDBCProductStorage();
 
-        storage.save(p1);
+        jdbcStorage.save(p1);
 
         assertThat(storage.allPublishedProducts())
                 .hasSize(1)
@@ -28,5 +39,9 @@ public class JDBCProductStorageTest {
         Product product = new Product(UUID.randomUUID());
         product.setPrice(BigDecimal.valueOf(10));
         return new Product(UUID.randomUUID());
+    }
+
+    private ProductStorage thereIsJDBCProductStorage() {
+        return new
     }
 }
