57b14a1c28bd62a831c1b152ca37a4559f93b25b
Kacper Surowka <kacper.surowka@pl.abb.com>
2021-01-03 11:29:31 +0100

    Added CRM

---
+++ src/main/java/pl/ksurowka/voucherstore/crm/ClientController.java
@@ -0,0 +1,32 @@
+package pl.ksurowka.voucherstore.crm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+import pl.ksurowka.voucherstore.productcatalog.JDBCProductStorage;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class ClientController {
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    @PostMapping("/api/clients")
+    public void addClient (@Valid @RequestBody Client client) {
+        clientRepository.save(client);
+    }
+
+    @GetMapping("/api/clients")
+    public List<Client> clients() {
+        return clientRepository.findAll();
+    }
+
+    @DeleteMapping("/api/clients/{id}")
+    public void deleteById(@Param("id") Integer id) {
+        clientRepository.deleteById(id);
+    }
+
+}
